1ffeb821043084aff06b8cc5842db6bb7ca3d685
Byron Conforme <andres99bcd@hotmail.com>
2021-02-18 22:28:58 -0500

    Cambio a bd personal, instalacion de express, CRUD representante_legal

--- node-project/network/routes.js
+++ node-project/network/routes.js
@@ -2,10 +2,12 @@ const express = require('express')
 const { route } = require('../components/usuario/network')
 const usuario = require('../components/usuario/network')
 const carrera = require('../components/carrera/network')
+const representante_legal = require('../components/representante_legal/network')
 
 const routes = function( server ) {
     server.use('/usuario', usuario)
     server.use('/carrera', carrera)
+    server.use('/representante_legal', representante_legal)
 }
 
 module.exports = routes
