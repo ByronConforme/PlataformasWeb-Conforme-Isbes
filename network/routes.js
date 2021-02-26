46535427620b0e7d334195279d57a95c1340880e
Byron Conforme <andres99bcd@hotmail.com>
2021-02-25 22:49:29 -0500

    Cambio en las rutas 2

--- network/routes.js
+++ network/routes.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { route } = require('./components/usuario/network')
-const usuario = require('./components/usuario/network')
+const { route } = require('../components/usuario/network')
+const usuario = require('../components/usuario/network')
 const carrera = require('./components/carrera/network')
-const representante_legal = require('./components/representante_legal/network')
-const institucion = require('./components/institucion/network')
+const representante_legal = require('../components/representante_legal/network')
+const institucion = require('../components/institucion/network')
 
 const routes = function( server ) {
     server.use('/usuario', usuario)
